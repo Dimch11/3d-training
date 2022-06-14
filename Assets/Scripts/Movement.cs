494df033d4a7bec9bee705a9da141d578570cd61
Dimch11 <87477131+Dimch11@users.noreply.github.com>
2022-06-15 00:13:41 +0400

    Jumping improvements

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -55,6 +55,8 @@ public class Movement : MonoBehaviour
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
     }
 
+    
+
     #region Move
 
     void Move()
