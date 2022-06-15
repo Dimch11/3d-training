3218f702067d13e3bcb9dfb68b7ee84bd3c7c2fb
Dimch11 <87477131+Dimch11@users.noreply.github.com>
2022-06-15 23:02:30 +0400

    Sky, ground texture, improved gravity

--- Assets/Old/Scripts/RoadGenerator.cs
+++ Assets/Old/Scripts/RoadGenerator.cs
@@ -18,7 +18,8 @@ public class RoadGenerator : MonoBehaviour
     {
         for (int i = 0; i < numOfRoadParts; i++)
         {
-            Instantiate(roadPartPrefab, firstRoadPart.position + new Vector3(10 * (i + 1), 0, 0), Quaternion.identity, roadParent);
+            var pos = firstRoadPart.position + new Vector3(10 * (i + 1), 0, 0);
+            Instantiate(roadPartPrefab, pos, roadPartPrefab.transform.rotation, roadParent);
         }
     }
 }
